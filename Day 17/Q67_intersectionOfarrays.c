73ef0f03e59c7a7c0a1912e24c42e0c779a53bdb
Ritesh Kumar <riteshkumae098765@gmail.com>
2026-06-17 20:34:36 +0530

    Day 17 Completed

---
+++ Day 17/Q67_intersectionOfarrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a[3], b[3];
+    int i, j;
+
+    printf("Enter 3 elements of first array:\n");
+    for (i = 0; i < 3; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter 3 elements of second array:\n");
+    for (i = 0; i < 3; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    printf("Intersection: ");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (a[i] == b[j]) {
+                printf("%d ", a[i]);
+            }
+        }
+    }
+
+    return 0;
+}
