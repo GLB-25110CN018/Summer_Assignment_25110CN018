a04dea598e9c7854a46dc2dcf8dba7ccc97d3e19
Ritesh Kumar <riteshkumae098765@gmail.com>
2026-06-19 22:25:48 +0530

    Day 19 Completed

---
+++ Day 19/Q76_findDiagonalsum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2];
+    int i, j, sum = 0;
+
+    printf("Enter matrix:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < 2; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Diagonal Sum = %d", sum);
+
+    return 0;
+}
