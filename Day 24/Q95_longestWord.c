ba915e481bc1a075579642b5fcda940121a7a542
Ritesh Kumar <riteshkumae098765@gmail.com>
2026-06-24 21:29:28 +0530

    Day 24 Completed

---
+++ Day 24/Q95_longestWord.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, len = 0, max = 0;
+
+    printf("Enter a sentence: ");
+    gets(str);
+
+    while(1) {
+        if(str[i] != ' ' && str[i] != '\0')
+            len++;
+        else {
+            if(len > max)
+                max = len;
+            len = 0;
+        }
+
+        if(str[i] == '\0')
+            break;
+
+        i++;
+    }
+
+    printf("Longest Word Length = %d", max);
+
+    return 0;
+}
